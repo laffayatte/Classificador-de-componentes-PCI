d9e4a7041343c175a06abad1d90c5e30ef726da3
João Victor Marques <49372857+laffayatte@users.noreply.github.com>
2022-05-06 20:53:31 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,122 @@
+# Classificação de Componentes Eletrônicos em Placa de Circuito Impresso utilizando Machine Learning
+
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/65060013/153782146-1fd45186-119c-47f7-9f10-44bd09e5a0b0.gif">
+</p>
+
+## 🪧 Descrição
+
+
+A aplicação tem como objetivo classificar e quantizar os tipos de componentes de uma placa de circuito impresso.
+
+## 🖥️ Tecnologias
+
+
+A aplicação web foi desenvolvida com auxílio do framework [Flask](https://flask.palletsprojects.com/en/2.0.x/), juntamente com bibliotecas para o processamento de imagens ([OpenCV](https://opencv.org/)) e para o uso da estruturas de rede neural artificial ([PyTorch](https://pytorch.org/)). Todos essas bibliotecas sendo da [Linguagem Python](https://www.python.org/) versão 3.9.7.
+
+## 🧾 Instalação de dependências
+
+
+Após clonar esse repositório em seu computador e ter a versão surgerida do Python, é necessário instalar as bibliotecas necessárias para o funcionamento da aplicação. São elas: 
+
+```python
+fonttools==4.29.0
+itsdangerous==2.0.1
+Jinja2==3.0.3
+kiwisolver==1.3.2
+MarkupSafe==2.0.1
+matplotlib==3.5.1
+numpy==1.21.4
+opencv-python==4.5.3.56
+packaging==21.3
+Pillow==9.0.0
+pyparsing==3.0.7
+python-dateutil==2.8.2
+six==1.16.0
+torch==1.8.0
+torchvision==0.9.0
+typing_extensions==4.0.1
+Werkzeug==2.0.2
+```
+
+Instalando via terminal com o comando [pip](https://pypi.org/project/pip/):
+
+```bash
+$ pip install -r requirements.txt
+```
+
+
+## 📟 Como usar
+
+
+Após a instalação de dependências, é preciso que usar o comando de iniciação da aplicação no terminal (ou no servidor web), com o comando:
+
+```python
+$ python .\app.py
+```
+
+Com isso, é possível visualizar a aplicação diretamente no navegador (ou no servidor web).
+
+## 🌳 Ramos da Aplicação
+
+### 📚 Trilha de Aprendizado
+
+
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/65060013/153949876-bc58cecd-b0a1-4eb5-9977-c5dfbdc431ba.gif">
+</p>
+
+
+
+Ramo responsável pela introdução de conteúdos, conceitos, definições para o usuário da plataforma.
+
+
+<hr>
+
+### 🛠️ Classificador de Componentes
+
+
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/65060013/153951837-932adc0b-93b7-48a5-947b-2e7befb3290f.gif">
+</p>
+
+
+Ramo que o usuário é capaz de fazer o upload de uma imagem para o classificador agir. O classificador foi treinado para detectar 4 tipos de componentes, são eles:
+
+- Fusível
+- Relê
+- Capacitor eletrolítico
+- LED
+
+## ✍️ Autores
+
+
+
+> *Fabrício da Costa Guimarães* 
+> - Engenharia da Computação UFAM 
+> - <a href="https://www.linkedin.com/in/abriciof/" target="_blank"> LinkedIn </a> 
+> - <a href="https://github.com/abriciof/" target="_blank"> GitHub </a>
+> 
+>
+> *João Victor de Carvalho Marques*
+> - Engenharia da Computação UFAM 
+> - <a href="https://www.linkedin.com/in/jo%C3%A3o-marques-171261134/" target="_blank"> LinkedIn </a>
+> - <a href="https://github.com/Battlesides" target="_blank"> GitHub </a>
+> 
+>
+> *Kluiwert Victor Tavares Mota* 
+> - Engenharia Elétrica UFAM
+> - <a href="https://www.linkedin.com/" target="_blank"> LinkedIn </a>
+> - <a href="https://github.com/kluiwert/" target="_blank"> GitHub </a>
+>
+
+
+
+<p align="center">
+  <br>
+  <img src="https://user-images.githubusercontent.com/65060013/156955108-35069a93-3c6e-4e07-abf8-dfd5f3a443bb.png" style="width:10%">
+    <img src="https://user-images.githubusercontent.com/65060013/156952397-8edfe44c-edfc-406c-bfb0-87f7a5e73e0b.png" style="width:10%">
+</p>
+
+
+
---
+++ app.py
@@ -0,0 +1,162 @@
+from logging import debug
+from flask import Flask, flash, render_template, request, jsonify
+from apps.main_torch import *
+from apps.main_cv2 import *
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import io
+import cv2
+# opencv-python==4.5.3.56
+
+
+
+app = Flask(__name__)
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'jfif'}
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    src=""
+    return render_template('index.html', aaaa=src)
+
+@app.route('/trilha', methods=['GET'])
+def trilha():
+    return render_template('home-trail.html')
+
+@app.route('/upload', methods=['GET'])
+def enviar():
+    return render_template('upload.html')
+
+@app.route('/classificacao', methods=['POST'])
+def previsao():
+    if request.method == 'POST':
+        file = request.files.get('file')
+        src = request.form.get('invisivel')  
+        # print(src)
+        if file is None or file.filename == '':
+            return render_template('upload.html', previsao="Sem arquivo.")
+            # return jsonify({'error': 'no file'})
+        if not allowed_file(file.filename):
+            # return jsonify({'error': 'format not supported'})
+            return render_template('upload.html', previsao="Formato não suportado.")
+        else:
+            img_bytes = file.read()
+            # print(type(img_bytes))
+            image = Image.open(io.BytesIO(img_bytes)) #imagem PIL
+            # print(type(image))
+            buff = np.fromstring(img_bytes, np.uint8)
+            # print(type(buff))
+            buff = buff.reshape(1, -1)
+            # print(type(buff))
+            img = cv2.imdecode(buff,  cv2.IMREAD_GRAYSCALE )
+            # print(type(img))
+            # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #imagem cv2 pronta para ser trabalhada
+            # print(type(img))
+
+    
+
+
+
+            # buff = np.fromstring(img_bytes, np.uint8)
+            # buff = buff.reshape(1, -1)
+            # img = cv2.imdecode(buff, cv2.IMREAD_COLOR)
+            # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #imagem cv2 pronta para ser trabalhada
+
+            lista_blocos = corta3x3(img)
+            lista_blocos_PIL = cv2_to_PIL(lista_blocos)
+            resultado = quantos_verificou(lista_blocos_PIL)
+
+            if resultado[0]>0:
+                fusivel = f'Quantidade de Fusível: {resultado[0]}'
+            else:
+                fusivel = 0
+
+            if resultado[1]>0:
+                rele = f'Quantidade de Relé: {resultado[1]}'
+            else:
+                rele = 0
+
+            if resultado[2]>0:
+                capacitor = f'Quantidade de Capacitor Eletrolítico: {resultado[2]}'
+            else:
+                capacitor = 0
+
+            if resultado[3]>0:
+                led = f'Quantidade de LED: {resultado[3]}'
+            else:
+                led = 0
+            
+
+            prev = 0
+            title = "1"
+
+
+            # fusivel, rele, capacitor, led = 0,0,0,0
+
+
+            # tensor = transform_image(image)
+            # img = Image.open(src)
+            # plt.imshow(img)
+            # return "Imagem transformada em tensor e pronto para a classificação: " + str(tensor)
+            # prev =  str(tensor)
+            return render_template('upload.html', previsao=prev, title=title, aaaa=src, result1=fusivel, result2=rele, result3=capacitor, result4=led)
+
+
+@app.route('/t1', methods=['GET'])
+def t1():
+    return render_template('trail-content-1.html')
+
+@app.route('/t2', methods=['GET'])
+def t2():
+    return render_template('trail-content-2.html')
+
+@app.route('/t3', methods=['GET'])
+def t3():
+    return render_template('trail-content-3.html')
+
+@app.route('/t4', methods=['GET'])
+def t4():
+    return render_template('trail-content-4.html')
+
+@app.route('/t5', methods=['GET'])
+def t5():
+    return render_template('trail-content-5.html')
+
+@app.route('/t6', methods=['GET'])
+def t6():
+    return render_template('trail-content-6.html')
+
+@app.route('/t7', methods=['GET'])
+def t7():
+    return render_template('trail-content-7.html')
+
+@app.route('/t8', methods=['GET'])
+def t8():
+    return render_template('trail-content-8.html')
+
+@app.route('/t9', methods=['GET'])
+def t9():
+    return render_template('trail-content-9.html')
+
+@app.route('/t10', methods=['GET'])
+def t10():
+    return render_template('trail-content-10.html')
+
+@app.route('/t11', methods=['GET'])
+def t11():
+    return render_template('trail-content-11.html')
+
+@app.route('/t12', methods=['GET'])
+def t12():
+    return render_template('trail-content-12.html')
+
+@app.route('/t13', methods=['GET'])
+def t13():
+    return render_template('trail-content-13.html')
+
+if __name__ == '__main__':
+    app.secret_key = 'ItIsASecret'
+    app.run( debug=True)
